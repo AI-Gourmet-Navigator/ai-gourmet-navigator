a748dc8f4b327a59c4e4909dd8c6cc68e3ff9c8b
yuki-o1o5 <jkksst1712@gmail.com>
2024-05-24 23:47:03 -0700

    chore: install shadcn/ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { CreatePost } from "@/app/_components/create-post";
 import { getServerAuthSession } from "@/server/auth";
 import { api } from "@/trpc/server";
+import { Button } from "@/components/ui/button";
 
 export default async function Home() {
   const hello = await api.post.hello({ text: "from tRPC" });
@@ -53,6 +54,7 @@ export default async function Home() {
             >
               {session ? "Sign out" : "Sign in"}
             </Link>
+            <Button>Default</Button>
           </div>
         </div>
 
