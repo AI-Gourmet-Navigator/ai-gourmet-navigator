9b05d6a47a9fa97864e1a69f50b6e7557aa607f1
yuki-o1o5 <jkksst1712@gmail.com>
2024-06-10 11:10:43 -0700

    chore: add env to storybook config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,5 +1,7 @@
-import type { StorybookConfig } from "@storybook/nextjs";
-import { env } from "../src/env";
+import type { StorybookConfig } from '@storybook/nextjs'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
@@ -14,10 +16,22 @@ const config: StorybookConfig = {
     name: '@storybook/nextjs',
     options: {},
   },
-  staticDirs: ["../public"],
+  staticDirs: ['../public'],
   env: (config) => ({
     ...config,
-    NEXT_PUBLIC_GOOGLE_MAP_API: env.NEXT_PUBLIC_GOOGLE_MAP_API,
+    DATABASE_URL: process.env.DATABASE_URL || '',
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL || '',
+    DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID || '',
+    DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET || '',
+    GOOGLE_MAP_API: process.env.GOOGLE_MAP_API || '',
+    G_HUB_ID: process.env.G_HUB_ID || '',
+    G_HUB_SECRET: process.env.G_HUB_SECRET || '',
+    FACEBOOK_CLIENT_ID: process.env.FACEBOOK_CLIENT_ID || '',
+    FACEBOOK_CLIENT_SECRET: process.env.FACEBOOK_CLIENT_SECRET || '',
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID || '',
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET || '',
+    NEXT_PUBLIC_GOOGLE_MAP_API: process.env.NEXT_PUBLIC_GOOGLE_MAP_API || '',
   }),
-};
-export default config;
+}
+
+export default config
