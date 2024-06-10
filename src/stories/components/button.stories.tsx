39f35fbdd95c9f90893313c570f51a0135b167ed
yuki-o1o5 <jkksst1712@gmail.com>
2024-06-09 19:17:37 -0700

    docs: change file structure

--- src/stories/components/button.stories.tsx
+++ src/stories/components/button.stories.tsx
@@ -1,9 +1,9 @@
 import type { Meta, StoryObj } from '@storybook/react'
-import { Button } from './button'
+import { Button } from '../../components/ui/button'
 import { action } from '@storybook/addon-actions'
 
 const meta: Meta<typeof Button> = {
-  title: 'Components/ui/button',
+  title: 'Components/Button',
   component: Button,
   tags: ['autodocs'],
   parameters: {
