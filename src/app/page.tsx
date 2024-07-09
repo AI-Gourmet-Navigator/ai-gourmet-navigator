85b07d24cb30417cf0624af1e9b16e37715c0eb6
yuki-o1o5 <jkksst1712@gmail.com>
2024-07-09 15:58:15 -0700

    docs: add fetch function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,29 @@
 import { Chat } from '@/components/chat'
+import { type Restaurants } from './api/preference/route'
 
-export default function Home() {
+export default async function Home() {
+  const searchRestaurantPreference = async (): Promise<Restaurants[]> => {
+    try {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_APP_URL}/api/preference/route`,
+      )
+      if (!res.ok) {
+        throw new Error('Network response was not ok')
+      }
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+      return await res.json()
+    } catch (error) {
+      console.error('Error fetching restaurant preferences:', error)
+      return []
+    }
+  }
+
+  const data = await searchRestaurantPreference()
+
+  console.log(data)
+
   return (
-    <div className="container mt-16 flex flex-col items-center justify-center gap-2 px-4">
+    <div className="container relative flex flex-col items-center justify-center gap-2 px-4">
       <Chat />
     </div>
   )
