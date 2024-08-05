4644ac8185f7ee7199a08016c463c83c6e856468
yuki-o1o5 <jkksst1712@gmail.com>
2024-08-04 18:06:16 -0700

    dosc: add storybook fot chatInput and ChatContainer

--- src/components/chatInput.tsx
+++ src/components/chatInput.tsx
@@ -46,7 +46,11 @@ export function ChatInput({ onSubmit, currentQuestionType }: ChatInputProps) {
           ref={textareaRef}
           className="max-h-52 resize-none px-3 py-[7px] sm:ml-4 md:ml-8 lg:ml-20"
         />
-        <Button className="px-3 sm:mr-4 sm:px-4 md:mr-8 lg:mr-20" type="submit">
+        <Button
+          className="px-3 sm:mr-4 sm:px-4 md:mr-8 lg:mr-20"
+          aria-label="submit"
+          type="submit"
+        >
           <PaperPlaneIcon />
         </Button>
       </form>
